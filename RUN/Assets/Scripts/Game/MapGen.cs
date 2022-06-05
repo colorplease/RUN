77590e4548e4c976e2e30f734d2aa9983078b9f2
colorplease <74271202+colorplease@users.noreply.github.com>
2022-06-05 09:10:44 -0700

    im dying inside

--- RUN/Assets/Scripts/Game/MapGen.cs
+++ RUN/Assets/Scripts/Game/MapGen.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class MapGen : MonoBehaviour
 {
     [SerializeField]GameObject testSpawn;
+    [SerializeField]float rotationAmount;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.K))
         {
-            Instantiate(testSpawn, transform.position, Quaternion.identity);
+            Instantiate(testSpawn, transform.position, Quaternion.Euler(0, rotationAmount, 0));
         }
     }
 }
